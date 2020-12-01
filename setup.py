13bcca1c3f81fb253cef60ee21ecd4c25693816d
pwwang <pwwang@pwwang.com>
2020-12-01 01:17:40 -0700

    0.0.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ readme = ''
 setup(
     long_description=readme,
     name='pipda',
-    version='0.0.3',
+    version='0.0.4',
     description='A framework for data piping in python',
     python_requires='==3.*,>=3.7.0',
     author='pwwang',
